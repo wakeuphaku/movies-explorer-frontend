3ee93ffac33b1a2f4aec1c36a49a5ffb2742c953
Dmitry Uchkin <not3hunnit@icloud.com>
2023-12-20 21:42:08 +0300

    3 проверка

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,9 +1,25 @@
 import './FilterCheckbox.css';
+import {useState} from "react";
 
 export default function FilterCheckbox () {
+
+    const [isShort, setIsShort] = useState(false);
+
+    const handleShortOff = () => {
+        setIsShort(true)
+    }
+
+    const handleShortOn = () => {
+        setIsShort(false)
+    }
+
     return (
         <section className='filter-checkbox'>
-            <button className='filter-checkbox__button'></button>
+            {!isShort ? (
+            <button className='filter-checkbox__button' onClick={handleShortOff}></button>
+                ) : (
+                <button className='filter-checkbox__button_off' onClick={handleShortOn}></button>
+            )}
             <p className='filter-checkbox__text'>Короткометражки</p>
         </section>
     )
